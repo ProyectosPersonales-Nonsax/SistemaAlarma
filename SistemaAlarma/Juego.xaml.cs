3a7e96fb606e379975feb5f0c5642db3560fafd8
Nonsax <raul.marin@iesdoctorbalmis.com>
2020-01-14 09:59:53 +0100

    Agregar archivos de proyecto.

---
+++ SistemaAlarma/Juego.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SistemaAlarma
+{
+    /// <summary>
+    /// Lógica de interacción para Juego.xaml
+    /// </summary>
+    public partial class Juego : Window
+    {
+        public Juego()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Random seed = new Random();
+                    Grid g = new Grid();
+                    TextBox tb = new TextBox();
+                    Viewbox vb = new Viewbox();
+                    vb.Child = tb;
+                    g.Children.Add(vb);
+                    tb.Text = seed.Next(1, 10).ToString();
+                    g.Background = Brushes.DarkGreen;
+                    g.Margin = new Thickness(1);
+                    g.Tag = i + j;
+                    Grid.SetColumn(g, i);
+                    Grid.SetRow(g, j);
+                    sudoku.Children.Add(g);
+                }
+            }
+        }
+    }
+}
---
+++ SistemaAlarma/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SistemaAlarma
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Inicial.Text = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Process p = new Process();
+            //p.StartInfo.FileName = "C:/Users/alumno/Desktop/Reproductor.exe";
+            //p.Start();
+            //p.WaitForExit();
+            //Holo.Text = "Funciona!!";
+
+            Juego juego = new Juego();
+            juego.Owner = this;
+            juego.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            juego.Show();
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
